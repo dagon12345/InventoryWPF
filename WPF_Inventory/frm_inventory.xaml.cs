abe49bfb2f08d972aee94234c56b31f7c1d8ac46
dagon12345 <lanceespina478@gmail.com>
2023-06-29 16:28:10 +0800

    AW

--- WPF_Inventory/frm_inventory.xaml.cs
+++ WPF_Inventory/frm_inventory.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MySql.Data.MySqlClient;
+using System.Windows.Threading;
 
 namespace WPF_Inventory
 {
@@ -32,13 +33,15 @@ namespace WPF_Inventory
         public void display()
         {
 
-            da = new MySqlDataAdapter("Select NameofStaff,Section,Division,Piece,TypeOfICTEquipment,Type,YearAcquired FROM db_inventory ORDER BY id DESC", con);
+            da = new MySqlDataAdapter("Select id,NameofStaff,Section,Division,Piece,TypeOfICTEquipment,Type,YearAcquired FROM db_inventory ORDER BY id DESC", con);
             dt = new DataTable();
             da.Fill(dt);
             datagrid.ItemsSource = dt.DefaultView;
             //this.datagrid.Columns[0].Visibility = Visibility.Hidden;
 
         }
+
+        private DispatcherTimer timer;
 
         public void statusbar()
         {
@@ -52,8 +55,8 @@ namespace WPF_Inventory
             da.Fill(dt);
             foreach (DataRow dr in dt.Rows)
             {
-                txtstatus1.Text = dr["UserID"].ToString();
-                txtusertypestatus1.Text = dr["Usertype"].ToString();
+                lbluserid.Text = dr["UserID"].ToString();
+                lblusertype.Text = dr["Usertype"].ToString();
 
 
             }
@@ -75,8 +78,37 @@ namespace WPF_Inventory
 
             txtnameofstaff.Focus();
 
+            if(txtnameofstaff.Text == "")
+            {
+                listBoxSuggestions.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                listBoxSuggestions.Visibility = Visibility.Visible;
+            }
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+
+
+            timerTextBlock.Visibility = Visibility.Hidden;
+
 
         }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timerTextBlock.Text = DateTime.Now.ToString("HH:mm:ss");
+
+            if (txtnameofstaff.Text == "")
+            {
+                listBoxSuggestions.Visibility = Visibility.Hidden;
+            }
+       
+        }
+
         public void clear()
         {
             txtnameofstaff.Clear();
@@ -137,7 +169,7 @@ namespace WPF_Inventory
                     cmd2.CommandText = "insert into db_activitylogs (Date,Time,UserID,Activity)values (@Date,@Time,@UserID,@Activity)";
                     cmd2.Parameters.AddWithValue("@Date", DateTime.Now);
                     cmd2.Parameters.AddWithValue("@Time", DateTime.Now.ToString("HH:mm:ss"));
-                    cmd2.Parameters.AddWithValue("@UserID", txtstatus1.Text);
+                    cmd2.Parameters.AddWithValue("@UserID", lbluserid.Text);
                     cmd2.Parameters.AddWithValue("@Activity", " has added data into the system. Number of Pieces: " + txtpiece.Text + " Type of ICT Equipment: " + txttypeofict.Text + " Hardware Type: " + cmbtype.Text  );
                     cmd2.ExecuteNonQuery();
 
@@ -163,6 +195,197 @@ namespace WPF_Inventory
         private void btnclear_Click(object sender, RoutedEventArgs e)
         {
             clear();
+        }
+
+        private void btnupdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtnameofstaff.Text == "" || txtsection.Text == "" || txtdivision.Text == "" || txtpiece.Text == "" || txttypeofict.Text == "" || cmbtype.Text == "" || datetimepicker.Text == "")
+            {
+                MessageBox.Show("Please select data you want to update","Warning",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+            
+            else
+            {
+
+                MySqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd = new MySqlCommand("update db_inventory set NameOfStaff=@NameOfStaff, Section=@Section, Division=@Division ,Piece=@Piece,TypeOfICTEquipment=@TypeOfICTEquipment,Type=@Type,YearAcquired=@YearAcquired WHERE id=@id", con);
+                cmd.Parameters.AddWithValue("@id", txtid.Text);
+                cmd.Parameters.AddWithValue("@NameOfStaff", txtnameofstaff.Text);
+                cmd.Parameters.AddWithValue("@Section", txtsection.Text);
+                cmd.Parameters.AddWithValue("@Division", txtdivision.Text);
+                cmd.Parameters.AddWithValue("@Piece", txtpiece.Text);
+                cmd.Parameters.AddWithValue("@TypeOfICTEquipment", txttypeofict.Text);
+                cmd.Parameters.AddWithValue("@Type", cmbtype.Text);
+                cmd.Parameters.AddWithValue("@YearAcquired", datetimepicker.Text);
+                cmd.ExecuteNonQuery();
+
+
+
+
+
+
+                ///LOGS
+                DateTime theDate = DateTime.Now;
+                theDate.ToString("yyyy-MM-dd hh:mm:ss");
+
+                MySqlCommand cmd2 = con.CreateCommand();
+                cmd2.CommandType = CommandType.Text;
+                cmd2.CommandText = "insert into db_activitylogs (Date,Time,UserID,Activity)values (@Date,@Time,@UserID,@Activity)";
+                cmd2.Parameters.AddWithValue("@Date", DateTime.Now);
+                cmd2.Parameters.AddWithValue("@Time", DateTime.Now.ToString("HH:mm:ss"));
+                cmd2.Parameters.AddWithValue("@UserID", lbluserid.Text);
+                cmd2.Parameters.AddWithValue("@Activity", "has UPDATED INVENTORY with the usertype " + lblusertype.Text + " and with the username of " + lbluserid.Text);
+                cmd2.ExecuteNonQuery();
+
+                display();
+                clear();
+
+
+
+
+                // display();
+                MessageBox.Show("Successfully Updated");
+
+            }
+            
+        }
+
+        private void datagrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (datagrid.SelectedItem != null)
+            {
+                DataRowView selectedRow = (DataRowView)datagrid.SelectedItem;
+                string id = selectedRow["id"].ToString(); // Replace "YourColumnName" with the actual column name
+                string nameofstaff = selectedRow["NameOfStaff"].ToString(); // Replace "YourColumnName" with the actual column name
+                string section = selectedRow["Section"].ToString(); // Replace "YourColumnName" with the actual column name
+                string division = selectedRow["Division"].ToString(); // Replace "YourColumnName" with the actual column name
+                string Piece = selectedRow["Piece"].ToString(); // Replace "YourColumnName" with the actual column name
+                string typeofict = selectedRow["TypeOfICTEquipment"].ToString(); // Replace "YourColumnName" with the actual column name
+                string type = selectedRow["Type"].ToString(); // Replace "YourColumnName" with the actual column name
+                string yearacquired = selectedRow["YearAcquired"].ToString(); // Replace "YourColumnName" with the actual column name
+
+                txtid.Text = id;
+                txtnameofstaff.Text = nameofstaff;
+                txtsection.Text = section;
+                txtdivision.Text = division;
+                txtpiece.Text = Piece;
+                txttypeofict.Text = typeofict;
+                cmbtype.Text = type;
+                datetimepicker.Text = yearacquired;
+            }
+        }
+
+        private void btndelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtnameofstaff.Text == "" || txtsection.Text == "" || txtdivision.Text == "" || txtpiece.Text == "" || txttypeofict.Text == "" || cmbtype.Text == "" || datetimepicker.Text == "")
+            {
+                MessageBox.Show("Retrieve some data before deleting","Retrieve",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+            else if (MessageBox.Show("Are you sure you want to delete this data?", "Delete", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+
+
+
+
+
+
+                MySqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd = new MySqlCommand("DELETE FROM db_inventory WHERE id=@id", con);
+                cmd.Parameters.AddWithValue("@id", txtid.Text);
+                cmd.ExecuteNonQuery();
+
+
+                //LOGS
+                DateTime theDate = DateTime.Now;
+                theDate.ToString("yyyy-MM-dd hh:mm:ss");
+
+                MySqlCommand cmd2 = con.CreateCommand();
+                cmd2.CommandType = CommandType.Text;
+                cmd2.CommandText = "insert into db_activitylogs (Date,Time,UserID,Activity)values (@Date,@Time,@UserID,@Activity)";
+                cmd2.Parameters.AddWithValue("@Date", DateTime.Now);
+                cmd2.Parameters.AddWithValue("@Time", DateTime.Now.ToString("HH:mm:ss"));
+                cmd2.Parameters.AddWithValue("@UserID", lbluserid.Text);
+                cmd2.Parameters.AddWithValue("@Activity", "has DELETED data in Inventory with the usertype " + lblusertype.Text + " and a username of " + lbluserid.Text);
+                cmd2.ExecuteNonQuery();
+
+                display();
+                clear();
+
+
+
+
+                // display();
+                MessageBox.Show("Data successfully deleted.");
+
+            }
+        }
+
+        private void ShowSuggestions(List<string> suggestions)
+        {
+            // Clear previous suggestions
+            listBoxSuggestions.Items.Clear();
+
+            // Add new suggestions to the ListBox
+            foreach (string suggestion in suggestions)
+            {
+                listBoxSuggestions.Items.Add(suggestion);
+            }
+        }
+
+        private void listBoxSuggestions_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBoxSuggestions.SelectedItem != null)
+            {
+                string selectedSuggestion = listBoxSuggestions.SelectedItem.ToString();
+                txtnameofstaff.Text = selectedSuggestion;
+                listBoxSuggestions.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private List<string> GetSuggestionsFromDatabase(string inputText)
+        {
+            List<string> suggestions = new List<string>();
+
+            // Establish a connection to your SQL database
+            using (MySqlConnection connection = new MySqlConnection(cs.DBcon))
+            {
+                connection.Open();
+
+                // Create a SQL command to query the database
+                MySqlCommand command = new MySqlCommand("SELECT nameofstaff FROM db_inventory WHERE nameofstaff LIKE @inputText", connection);
+                command.Parameters.AddWithValue("@inputText", "%" + inputText + "%");
+
+                // Execute the SQL command and retrieve the suggestions
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string suggestion = reader.GetString(0);
+                        suggestions.Add(suggestion);
+                    }
+                }
+                listBoxSuggestions.Visibility = Visibility.Visible;
+            }
+
+            return suggestions;
+        }
+
+        private void txtnameofstaff_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string inputText = txtnameofstaff.Text;
+
+            // Perform database query based on the input text
+            List<string> suggestions = GetSuggestionsFromDatabase(inputText);
+
+            // Display the suggestions
+            ShowSuggestions(suggestions);
+        }
+
+        private void txtnameofstaff_MouseLeave(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
