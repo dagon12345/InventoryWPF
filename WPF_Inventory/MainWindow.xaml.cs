b5691f46e5d56a38b927f6ab35afdba490a486ed
dagon12345 <lanceespina478@gmail.com>
2023-07-03 16:16:36 +0800

    new update again

--- WPF_Inventory/MainWindow.xaml.cs
+++ WPF_Inventory/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -34,20 +36,131 @@ namespace WPF_Inventory
         MySqlCommand cmd;
         MySqlDataAdapter da;
         DataTable dt;
+
+        BackgroundWorker _bgWorker;
+        bool _iNeedToCloseAfterBgWorker;
+
+
+
+
+
+        void _bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ////completed here
+            lblnetstatus.Text = "Successfully connected to SQL Server.";
+            lblnetstatus.Foreground = Brushes.SeaGreen;
+            btninsert.IsEnabled = true;
+            btndelete.IsEnabled = true;
+            // btn_rc.Enabled = true;
+            if (_iNeedToCloseAfterBgWorker)
+                Close();
+        }
+
+
+        void _bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Access lbl_internet here
+
+
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                // Your UI-related code here
+                lblnetstatus.Text = "Detecting SQL Connection please wait...";
+                lblnetstatus.Foreground = Brushes.Yellow;
+            });
+
+
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                // Your UI-related code here
+                btninsert.IsEnabled = false;
+                btndelete.IsEnabled = false;
+            });
+
+
+
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                // Your UI-related code here
+               // panel.IsEnabled = false;
+            });
+
+
+
+            // Do long lasting work above is the before process before final
+            Thread.Sleep(1000);
+
+
+
+            try
+            {
+
+
+                con = new MySqlConnection(cs.DBcon);
+
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                con.Open();
+                display();
+                statusbar();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    // Your UI-related code here
+                    lblnetstatus.Text = "Can't connect to SQL host.";
+                    lblnetstatus.Foreground = Brushes.Crimson;
+                });
+
+                MessageBox.Show(ex.Message);
+
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    // Your UI-related code here
+                    Application.Current.Shutdown();
+                });
+
+            }
+        }
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
             lbl_id.Visibility = Visibility.Hidden;
+
+            _bgWorker = new BackgroundWorker();
+            _bgWorker.DoWork += _bgWorker_DoWork;
+            _bgWorker.RunWorkerCompleted += _bgWorker_RunWorkerCompleted;
+
+            _bgWorker.RunWorkerAsync();
+
+
         }
 
         public void display()
         {
-        
+
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+               
+          
+
             da = new MySqlDataAdapter("Select * from db_register ORDER BY id DESC", con);
             dt = new DataTable();
             da.Fill(dt);
             datagrid.ItemsSource = dt.DefaultView;
             this.datagrid.Columns[0].Visibility = Visibility.Hidden;
+
+            });
 
         }
 
@@ -75,8 +188,19 @@ namespace WPF_Inventory
             da.Fill(dt);
             foreach (DataRow dr in dt.Rows)
             {
-                txtstatus1.Text = dr["UserID"].ToString();
-                txtusertypestatus1.Text = dr["Usertype"].ToString();
+
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+
+
+
+                    txtstatus1.Text = dr["UserID"].ToString();
+                    txtusertypestatus1.Text = dr["Usertype"].ToString();
+
+                });
+
+
+        
 
 
             }
@@ -84,15 +208,7 @@ namespace WPF_Inventory
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            con = new MySqlConnection(cs.DBcon);
-
-            if (con.State == ConnectionState.Open)
-            {
-                con.Close();
-            }
-            con.Open();
-            display();
-            statusbar();
+            
             btninsert.Content = "Register";
             txtofficeid.Focus();
         }
